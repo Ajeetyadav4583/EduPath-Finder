f501c5cf399d2e385960d437600f61743f903996
Ajeet <233933245+Ajeetyadav4583@users.noreply.github.com>
2026-05-17 05:43:59 +0530

    fixed port binding

--- app.py
+++ app.py
@@ -247,6 +247,8 @@ def login():
 
 with app.app_context():
     db.create_all()
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
